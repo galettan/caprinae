f134dce5a779e29b71bd400c1abf87cb9cffeb87
gaetan <gaetanciq@gmail.com>
2015-10-16 17:04:41 +0200

    add methods to handle profile etc

--- app/models/project.rb
+++ app/models/project.rb
@@ -10,6 +10,18 @@ class Project < ActiveRecord::Base
   
   enum project_type: [:print, :crea, :printcrea]
 
+  def self.by_owner(id)
+      @projects = Project.where(owner_id:  id)
+  end
+  
+  def self.by_worker(id)
+    @projects = Project.where(worker_id: id)
+  end
+  
+  def self.by_profile(id, profile)
+    @projects = Project.where(["worker_id = ? AND project_type IN (?,?)", id, profile, 2])
+  end
+
   def self.find_detailed(id)
       @project = Project.includes(:owner, :worker, :contact).find(id)
   end
