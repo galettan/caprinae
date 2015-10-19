d802906d4f7e7adee283791a3e5bf916326a1e3e
gaetan <gaetanciq@gmail.com>
2015-10-19 23:04:21 +0200

    add new features and logo

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,4 +6,8 @@ validates :last_name, presence: true
 validates :first_name, presence: true
 validates :email, presence: true
 
+def first_name_and_last_name
+  "#{first_name} #{last_name}"
+end
+
 end
