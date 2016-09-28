1fbb537c76e70fadc92acb0c3a63fa2864cad7f7
gaetan <gaetanciq@gmail.com>
2016-09-28 21:09:24 +0200

    handle errors as 404

--- Gemfile
+++ Gemfile
@@ -50,6 +50,10 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem "paperclip", "~> 4.3"
 
+gem "figaro"
+
+gem 'wicked_pdf'
+gem 'wkhtmltopdf-binary'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  rescue_from Exception, :with => :render_404
+
+  private
+
+  def render_404
+    render :template => 'error_pages/404', :layout => false, :status => :not_found
+  end
 end
