d670901ce4e3400ce1773e7da013a9d51b787931
gaetan <gaetan.ciquie@gmail.com>
2017-06-26 22:33:55 +0200

    Bugfix - modify migration script

--- db/migrate/20170626193004_add_note_to_projects.rb
+++ db/migrate/20170626193004_add_note_to_projects.rb
@@ -1,9 +1,8 @@
-class AddNoteToProjects < ActiveRecord::Migration[5.0]
+class AddNoteToProjects < ActiveRecord::Migration
   def change
     reversible do |dir|
       change_table :projects do |t|
         dir.up {t.text :note}
-
         dir.down {t.remove :note}
       end
     end
