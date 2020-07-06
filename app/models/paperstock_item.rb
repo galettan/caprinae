8db129d90be8999571caaf6fa96daec294ae06e7
Gaetan Ciquie <gaetanciq@gmail.com>
2020-07-06 07:34:48 +0200

    add default value for paperstock

--- app/models/paperstock_item.rb
+++ app/models/paperstock_item.rb
@@ -3,5 +3,12 @@ class PaperstockItem < ActiveRecord::Base
 belongs_to :paperstock, class_name: Paperstock, foreign_key: "paperstock_id"
 
 validates :name, presence: true
+before_update :set_defaults
+
+    def set_defaults
+      self.thirtytwofortyfive = 0 if self.thirtytwofortyfive.nil?
+      self.fiftytwoseventytwo = 0 if self.fiftytwoseventytwo.nil?
+      self.toorder = 0 if self.toorder.nil?
+    end
 
 end
